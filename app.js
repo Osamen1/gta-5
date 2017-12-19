35eef3ccced0622969587a3e903a1921dd9308c2
Edan <EdanEA@users.noreply.github.com>
2017-12-19 17:38:42 -0600

    1.7.0
    xD

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const k = require('./secretspoops.json');
 global.Eris = require('eris');
-global.client = new Eris.Client(k.token, { connectionTimeout: 60000 });
+require("eris-additions")(Eris, { disabled: ["Channel.sendMessage", "Channel.sendCode", "Eris.Embed"] })
 global.sql = require('sqlite');
 global.fs = require('fs');
 global.c = require('chalk');
 global.moment = require('moment');
 global.snek = require('snekfetch');
+require('moment-duration-format');
+
+global.k = require('./secretspoops.json');
 global.perms = require('./functions/permissions.js');
 global.misc = require('./functions/misc.js');
-require('moment-duration-format');
+global.prefixes = require('./prefixes.json');
 require('./guild.js');
 
-global.prefix = "(";
+global.client = new Eris.Client(k.token, { connectionTimeout: 60000 });
 global.admins = { "josh":"117728104935456770", "hunter":"228963688910946304", "john":"148958241378926593" };
 global.owner = { "id":"221740788462256138" };
-global.bot = {"id": "305602159741763585", "serverlog":"382001924251320322", "suggestchannel":"382001960984772609", "testingserver":"312667247808217088", "officialserver":"380310916341956610"};
+global.bot = {"id": "305602159741763585", "serverlog":"382001924251320322", "suggestchannel":"382001960984772609", "testingserver":"312667247808217088", "officialserver":"380310916341956610", "prefix":")"};
 
 sql.open('./stats.sqlite');
 
@@ -26,9 +28,25 @@ process.on('unhandledRejection', (reason, p) => {
   console.log(`Unhandled rejection at: ${p} reason: ${reason}`);
 });
 
+process.stdin.resume();
+
+process.on('exit', (code) => {
+  console.log(`Exited with code ${code}`);
+});
+
+process.on('SIGINT', () => {
+  fs.writeFile('./prefixes.json', JSON.stringify(prefixes), 'utf8', (err) => {
+    if(err) throw err;
+    else { console.log(`Successfully logged ${Object.keys(prefixes).length} prefixes.`); process.exit(); }
+  });
+});
+
 client.on("ready", () => {
   console.log(c.blue(`Damn Daniel.\nServer Amount: ${client.guilds.size}`));
   misc.randomStatus();
+  client.guilds.forEach(g => {
+    if(!prefixes[g.id]) prefixes[g.id] = ")"
+  });
   new snek('POST', `https://bots.discord.pw/api/bots/${bot.id}/stats`).set('Authorization', k.bdt).send({ server_count: client.guilds.size }).then(() => { console.log("Sent server amount to bots.discord.pw successfully.") }).catch(e => { if(e) throw e.stack; return; });
   new snek('POST', `https://discordbots.org/api/bots/${bot.id}/stats`).set('Authorization', k.dblt).send({ server_count: client.guilds.size }).then(() => { console.log("Sent server amount to discordbots.org successfully.") }).catch(e => { if(e) throw e.stack; return; });
 });
@@ -36,6 +54,7 @@ client.on("ready", () => {
 client.on("guildCreate", guild => {
   console.log(c.yellow(`I've joined ${guild.name}. Server amount: ${client.guilds.size}.`));
   guild.defaultChannel.createMessage(`\`\`\`Hey, I'm Russian roulette. ~~I know, it's a shitty name; blame the developer.~~ I'm just a bot, mainly used to play Russian roulette, but I also have some fun commands, and a lot more coming. We're always looking for new feautures, so if you have an idea use the ")suggest" command -- you could also join the discord server and tell the developer himself. Server Invite: https://discord.me/xdd.\`\`\``);
+  prefixes[guild.id] = ")";
   try {
     client.guilds.get(bot.officialserver).channels.get(bot.serverlog).createMessage(`I've joined ${guild.name}. Server amount: ${client.guilds.size}.`);
   } catch (e) {
@@ -56,10 +75,15 @@ client.on('guildDelete', guild => {
   new snek('POST', `https://discordbots.org/api/bots/${bot.id}/stats`).set('Authorization', k.dblt).send({ server_count: client.guilds.size }).then(() => { console.log("Sent server amount to discordbots.org successfully.") }).catch(e => { if(e) throw e.stack; return; });
 });
 
+
 client.on("messageCreate", (message) => {
-  if(!message.content.startsWith(prefix)) return;
+  var prefix;
+  if(!message.content.startsWith(prefixes[message.channel.guild.id]) && !message.content.startsWith(bot.prefix)) return;
   if(message.author == client.user) return;
-  if(!message.channel.guild) return message.channel.createMessage("no, fuck off. go use me in an actual guild.");
+  if(!message.channel.guild) return;
+
+  if(message.content.startsWith(prefixes[message.channel.guild.id])) var prefix = prefixes[message.channel.guild.id];
+  else var prefix = bot.prefix;
 
   let cmd = message.content.slice(prefix.length).toLowerCase().split(' ')[0];
   let args = message.content.split(' ').slice(1);
@@ -79,4 +103,4 @@ setInterval(misc.randomStatus, 1800000);
 setInterval(misc.dailyBackup, 86400000);
 
 client.connect();
-// xD I'm killing myself
+// ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
