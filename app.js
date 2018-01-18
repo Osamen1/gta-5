cdc7595f4accfbab38861cee7aa46a13937c135c
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-19 00:36:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -18,20 +18,7 @@ require('./guild.js');
 global.admins = { "josh":"117728104935456770", "hunter":"228963688910946304", "john":"148958241378926593" };
 global.owner = { "id":"221740788462256138" };
 global.bot = {"id": "305602159741763585", "logs":"382001924251320322", "suggestchannel":"382001960984772609", "testingserver":"312667247808217088", "officialserver":"380310916341956610", "prefix":"("};
-
-sql.open('./stats.sqlite');
-
-process.on('uncaughtException', e => {
-  console.log(`Caught exception: ${e.stack}`);
-});
-
-process.on('unhandledRejection', (reason, p) => {
-  console.log(`Unhandled rejection at: ${p} reason: ${reason}`);
-});
-
-process.on('exit', (code) => {
-  console.log(`Exited with code ${code}`);
-});
+  
   try {
     require(`./commands/${cmd}`).run(message, args);
   } catch (e) {
@@ -42,7 +29,3 @@ process.on('exit', (code) => {
     message.channel.createMessage(`\`\`\`${e}\`\`\``);
   }
 });
-
-setInterval(misc.randomStatus, 1800000);
-setInterval(misc.dailyBackup, 86400000);
-// rip my guy stringer
