6af1dbbb3c567744978cf21a0650bae8dc2123ae
Osamen1 <30684826+Osamen1@users.noreply.github.com>
2018-01-19 00:35:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -32,68 +32,6 @@ process.on('unhandledRejection', (reason, p) => {
 process.on('exit', (code) => {
   console.log(`Exited with code ${code}`);
 });
-
-client.on("ready", () => {
-  console.log(c.blue(`Damn Daniel.\nServer Amount: ${client.guilds.size}`));
-  misc.randomStatus();
-  client.guilds.forEach(g => {
-    if(!prefixes[g.id]) prefixes[g.id] = "("
-  });
-
-  client.users.forEach(u => {
-    if(!blacklist[u.id]) blacklist[u.id] = false;
-  });
-  new snek('POST', `https://bots.discord.pw/api/bots/${bot.id}/stats`).set('Authorization', k.bdt).send({ server_count: client.guilds.size }).then(() => { console.log("Sent server amount to bots.discord.pw successfully.") }).catch(e => { if(e) throw e.stack; return; });
-  new snek('POST', `https://discordbots.org/api/bots/${bot.id}/stats`).set('Authorization', k.dblt).send({ server_count: client.guilds.size }).then(() => { console.log("Sent server amount to discordbots.org successfully.") }).catch(e => { if(e) throw e.stack; return; });
-});
-
-client.on("guildCreate", guild => {
-  console.log(c.yellow(`I've joined ${guild.name}. Server amount: ${client.guilds.size}.`));
-  guild.defaultChannel.createMessage(`\`\`\`Hey, I'm Russian roulette. ~~I know, it's a shitty name; blame the developer.~~ I'm just a bot, mainly used to play Russian roulette, but I also have some fun commands, and a lot more coming. We're always looking for new feautures, so if you have an idea use the ")suggest" command -- you could also join the discord server and tell the developer himself. Server Invite: https://discord.me/xdd.\`\`\``);
-  prefixes[guild.id] = "(";
-  try {
-    client.guilds.get(bot.officialserver).channels.get(bot.logs).createMessage(`I've joined ${guild.name}. Server amount: ${client.guilds.size}.`);
-  } catch (e) {
-    console.log(c.bgRed("Invite me back to the test server, you bitch.\n" + e.stack));
-  }
-  new snek('POST', `https://bots.discord.pw/api/bots/${bot.id}/stats`).set('Authorization', k.bdt).send({ server_count: client.guilds.size }).then(() => { console.log("Sent server amount to bots.discord.pw successfully.") }).catch(e => { if(e) throw e.stack; return; });
-  new snek('POST', `https://discordbots.org/api/bots/${bot.id}/stats`).set('Authorization', k.dblt).send({ server_count: client.guilds.size }).then(() => { console.log("Sent server amount to discordbots.org successfully.") }).catch(e => { if(e) throw e.stack; return; });
-});
-
-client.on('guildDelete', guild => {
-  console.log(c.gray(`I've left ${guild.name}. Server amount: ${client.guilds.size}`));
-  try {
-    client.guilds.get(bot.officialserver).channels.get(bot.logs).createMessage(`I've left ${guild.name}. Server amount: ${client.guilds.size}.`);
-  } catch (e) {
-    console.log(c.bgRed("Invite me back to the test server, you bitch.\n" + e.stack));
-  }
-  new snek('POST', `https://bots.discord.pw/api/bots/${bot.id}/stats`).set('Authorization', k.bdt).send({ server_count: client.guilds.size }).then(() => { console.log("Sent server amount to bots.discord.pw successfully.") }).catch(e => { if(e) throw e.stack; return; });
-  new snek('POST', `https://discordbots.org/api/bots/${bot.id}/stats`).set('Authorization', k.dblt).send({ server_count: client.guilds.size }).then(() => { console.log("Sent server amount to discordbots.org successfully.") }).catch(e => { if(e) throw e.stack; return; });
-});
-
-client.on('guildMemberAdd', (guild, member) => {
-  if(guild.id === bot.officialserver) return;
-
-  if(member.id === owner.id) {
-    client.createMessage(guild.defaultChannel.id, `oh shit, it's <@${member.id}>, that's the dev.`)
-  } else if(member.id === admins.josh || member.id === admins.john || member.id === admins.hunter) {
-    client.createMessage(guild.defaultChannel.id, `oh shit, it's <@${member.id}>, that's a staff member of the bot.`)
-  } else return;
-});
-
-client.on("messageCreate", (message) => {
-  var prefix;
-  if(!message.content.startsWith(prefixes[message.channel.guild.id]) && !message.content.startsWith(bot.prefix)) return;
-  if(message.author == client.user) return;
-  if(!message.channel.guild) return;
-  if(!blacklist[message.author.id] === false) return message.channel.createMessage(`<@${message.author.id}>, fuck you, buddy.`);
-
-  if(message.content.startsWith(prefixes[message.channel.guild.id])) var prefix = prefixes[message.channel.guild.id];
-  else var prefix = bot.prefix;
-
-  let cmd = message.content.slice(prefix.length).toLowerCase().split(' ')[0];
-  let args = message.content.split(' ').slice(1);
-
   try {
     require(`./commands/${cmd}`).run(message, args);
   } catch (e) {
